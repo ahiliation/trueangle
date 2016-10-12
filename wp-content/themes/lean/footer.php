9f6607b4e3d8651d929eb71913d95544116cf0e7
Jeffrin Jose T <ahiliation@yahoo.co.in>
2016-10-13 00:52:33 +0530

    update related

--- wp-content/themes/lean/footer.php
+++ wp-content/themes/lean/footer.php
@@ -1,7 +1,7 @@
 
 
 	<div id="footer">
-		Copyright &copy; ,&nbsp;2016 &nbsp; &nbsp; Trueangle
+		Copyright &copy; &nbsp;2016 &nbsp; &nbsp; Trueangle
 		 Project &nbsp;&nbsp; e-mail:&nbsp; ahiliation@yahoo.co.in
 		 <br>
 		 Trueangle Project comes with ABSOLUTELY NO WARRANTY, to the extent
